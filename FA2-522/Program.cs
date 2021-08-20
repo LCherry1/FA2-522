d55c5cb4048a470f7be8cc23ddedc5aae013dce6
LCherry1 <logancherry01@gmail.com>
2021-08-20 08:23:36 +0200

    Add project files.

---
+++ FA2-522/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FA2_522
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter username:");
+
+            string userName = Console.ReadLine();
+
+            // Print the value of the variable (userName), which will display the input value
+            Console.WriteLine("Username is: " + userName);
+        }
+    }
+}
